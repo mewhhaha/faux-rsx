280aafb89e8184a1798d14e1b95192413d13be48
Jacob Torrång <kopatheonlyone@hotmail.com>
2024-01-10 22:10:29 +0100

    Add properties

--- game/src/main.rs
+++ game/src/main.rs
@@ -29,7 +29,9 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let apple = "banana";
 
     render(
-        rsx! {<div>"hello"<div>"world"</div></div>},
+        rsx! {<div class="flex bg-black w-64">
+            "hello"
+        <div>"world"</div></div>},
         &mut commands,
         root,
         &font,
@@ -43,6 +45,7 @@ const ASPECT_SQUARE: f32 = 1.0;
 struct DefaultStyleSheet {}
 
 const REM: f32 = 16.0;
+const PIXEL_MULTIPLIER: f32 = REM / 4.0;
 
 impl Stylesheet for DefaultStyleSheet {
     fn get_style(&self, names: &str) -> Option<Style> {
@@ -52,6 +55,9 @@ impl Stylesheet for DefaultStyleSheet {
 
         for name in names.split(" ") {
             match name {
+                "block" => style.display = Display::None,
+                "grid" => style.display = Display::Grid,
+                "flex" => style.display = Display::Flex,
                 "flex-row" => style.flex_direction = FlexDirection::Row,
                 "flex-col" => style.flex_direction = FlexDirection::Column,
                 "flex-row-reverse" => style.flex_direction = FlexDirection::RowReverse,
@@ -90,7 +96,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "w-44" | "w-48" | "w-52" | "w-56" | "w-60" | "w-64" | "w-72" | "w-80"
                 | "w-96" => {
                     let value = name["w-".len()..].parse::<f32>().unwrap_or(1.0);
-                    style.width = Val::Px(value * REM)
+                    style.width = Val::Px(value * PIXEL_MULTIPLIER)
                 }
                 "w-1/2" | "w-1/3" | "w-2/3" | "w-1/4" | "w-2/4" | "w-3/4" | "w-1/5" | "w-2/5"
                 | "w-3/5" | "w-4/5" | "w-1/6" | "w-2/6" | "w-3/6" | "w-4/6" | "w-5/6"
@@ -107,7 +113,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "h-44" | "h-48" | "h-52" | "h-56" | "h-60" | "h-64" | "h-72" | "h-80"
                 | "h-96" => {
                     let value = name["h-".len()..].parse::<f32>().ok()?;
-                    style.height = Val::Px(value * REM);
+                    style.height = Val::Px(value * PIXEL_MULTIPLIER);
                 }
                 "h-1/2" | "h-1/3" | "h-2/3" | "h-1/4" | "h-2/4" | "h-3/4" | "h-1/5" | "h-2/5"
                 | "h-3/5" | "h-4/5" | "h-1/6" | "h-2/6" | "h-3/6" | "h-4/6" | "h-5/6"
@@ -156,7 +162,8 @@ impl Stylesheet for DefaultStyleSheet {
                 | "p-44" | "p-48" | "p-52" | "p-56" | "p-60" | "p-64" | "p-72" | "p-80"
                 | "p-96" => {
                     let value = name["p-".len()..].parse::<f32>();
-                    style.padding = UiRect::all(Val::Px(value.map(|v| v * REM).unwrap_or(1.0)))
+                    style.padding =
+                        UiRect::all(Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0)))
                 }
                 "pt-0" | "pt-px" | "pt-0.5" | "pt-1" | "pt-1.5" | "pt-2" | "pt-2.5" | "pt-3"
                 | "pt-3.5" | "pt-4" | "pt-5" | "pt-6" | "pt-7" | "pt-8" | "pt-9" | "pt-10"
@@ -164,7 +171,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "pt-36" | "pt-40" | "pt-44" | "pt-48" | "pt-52" | "pt-56" | "pt-60" | "pt-64"
                 | "pt-72" | "pt-80" | "pt-96" => {
                     let value = name["pt-".len()..].parse::<f32>();
-                    style.padding.top = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.padding.top = Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "pr-0" | "pr-px" | "pr-0.5" | "pr-1" | "pr-1.5" | "pr-2" | "pr-2.5" | "pr-3"
                 | "pr-3.5" | "pr-4" | "pr-5" | "pr-6" | "pr-7" | "pr-8" | "pr-9" | "pr-10"
@@ -172,7 +179,8 @@ impl Stylesheet for DefaultStyleSheet {
                 | "pr-36" | "pr-40" | "pr-44" | "pr-48" | "pr-52" | "pr-56" | "pr-60" | "pr-64"
                 | "pr-72" | "pr-80" | "pr-96" => {
                     let value = name["pr-".len()..].parse::<f32>();
-                    style.padding.right = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.padding.right =
+                        Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "pb-0" | "pb-px" | "pb-0.5" | "pb-1" | "pb-1.5" | "pb-2" | "pb-2.5" | "pb-3"
                 | "pb-3.5" | "pb-4" | "pb-5" | "pb-6" | "pb-7" | "pb-8" | "pb-9" | "pb-10"
@@ -180,7 +188,8 @@ impl Stylesheet for DefaultStyleSheet {
                 | "pb-36" | "pb-40" | "pb-44" | "pb-48" | "pb-52" | "pb-56" | "pb-60" | "pb-64"
                 | "pb-72" | "pb-80" | "pb-96" => {
                     let value = name["pb-".len()..].parse::<f32>();
-                    style.padding.bottom = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.padding.bottom =
+                        Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "pl-0" | "pl-px" | "pl-0.5" | "pl-1" | "pl-1.5" | "pl-2" | "pl-2.5" | "pl-3"
                 | "pl-3.5" | "pl-4" | "pl-5" | "pl-6" | "pl-7" | "pl-8" | "pl-9" | "pl-10"
@@ -188,7 +197,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "pl-36" | "pl-40" | "pl-44" | "pl-48" | "pl-52" | "pl-56" | "pl-60" | "pl-64"
                 | "pl-72" | "pl-80" | "pl-96" => {
                     let value = name["pl-".len()..].parse::<f32>();
-                    style.padding.left = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.padding.left = Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "px-0" | "px-px" | "px-0.5" | "px-1" | "px-1.5" | "px-2" | "px-2.5" | "px-3"
                 | "px-3.5" | "px-4" | "px-5" | "px-6" | "px-7" | "px-8" | "px-9" | "px-10"
@@ -196,7 +205,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "px-36" | "px-40" | "px-44" | "px-48" | "px-52" | "px-56" | "px-60" | "px-64"
                 | "px-72" | "px-80" | "px-96" => {
                     let number = name["px-".len()..].parse::<f32>();
-                    let value = Val::Px(number.map(|v| v * REM).unwrap_or(1.0));
+                    let value = Val::Px(number.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0));
                     style.padding.left = value;
                     style.padding.right = value;
                 }
@@ -206,7 +215,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "py-36" | "py-40" | "py-44" | "py-48" | "py-52" | "py-56" | "py-60" | "py-64"
                 | "py-72" | "py-80" | "py-96" => {
                     let number = name["py-".len()..].parse::<f32>();
-                    let value = Val::Px(number.map(|v| v * REM).unwrap_or(1.0));
+                    let value = Val::Px(number.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0));
                     style.padding.top = value;
                     style.padding.bottom = value;
                 }
@@ -216,7 +225,8 @@ impl Stylesheet for DefaultStyleSheet {
                 | "m-44" | "m-48" | "m-52" | "m-56" | "m-60" | "m-64" | "m-72" | "m-80"
                 | "m-96" => {
                     let value = name["m-".len()..].parse::<f32>();
-                    style.margin = UiRect::all(Val::Px(value.map(|v| v * REM).unwrap_or(1.0)))
+                    style.margin =
+                        UiRect::all(Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0)))
                 }
                 "mt-0" | "mt-px" | "mt-0.5" | "mt-1" | "mt-1.5" | "mt-2" | "mt-2.5" | "mt-3"
                 | "mt-3.5" | "mt-4" | "mt-5" | "mt-6" | "mt-7" | "mt-8" | "mt-9" | "mt-10"
@@ -224,7 +234,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "mt-36" | "mt-40" | "mt-44" | "mt-48" | "mt-52" | "mt-56" | "mt-60" | "mt-64"
                 | "mt-72" | "mt-80" | "mt-96" => {
                     let value = name["mt-".len()..].parse::<f32>();
-                    style.margin.top = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.margin.top = Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "mr-0" | "mr-px" | "mr-0.5" | "mr-1" | "mr-1.5" | "mr-2" | "mr-2.5" | "mr-3"
                 | "mr-3.5" | "mr-4" | "mr-5" | "mr-6" | "mr-7" | "mr-8" | "mr-9" | "mr-10"
@@ -232,7 +242,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "mr-36" | "mr-40" | "mr-44" | "mr-48" | "mr-52" | "mr-56" | "mr-60" | "mr-64"
                 | "mr-72" | "mr-80" | "mr-96" => {
                     let value = name["mr-".len()..].parse::<f32>();
-                    style.margin.right = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.margin.right = Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "mb-0" | "mb-px" | "mb-0.5" | "mb-1" | "mb-1.5" | "mb-2" | "mb-2.5" | "mb-3"
                 | "mb-3.5" | "mb-4" | "mb-5" | "mb-6" | "mb-7" | "mb-8" | "mb-9" | "mb-10"
@@ -240,7 +250,8 @@ impl Stylesheet for DefaultStyleSheet {
                 | "mb-36" | "mb-40" | "mb-44" | "mb-48" | "mb-52" | "mb-56" | "mb-60" | "mb-64"
                 | "mb-72" | "mb-80" | "mb-96" => {
                     let value = name["mb-".len()..].parse::<f32>();
-                    style.margin.bottom = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.margin.bottom =
+                        Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "ml-0" | "ml-px" | "ml-0.5" | "ml-1" | "ml-1.5" | "ml-2" | "ml-2.5" | "ml-3"
                 | "ml-3.5" | "ml-4" | "ml-5" | "ml-6" | "ml-7" | "ml-8" | "ml-9" | "ml-10"
@@ -248,7 +259,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "ml-36" | "ml-40" | "ml-44" | "ml-48" | "ml-52" | "ml-56" | "ml-60" | "ml-64"
                 | "ml-72" | "ml-80" | "ml-96" => {
                     let value = name["ml-".len()..].parse::<f32>();
-                    style.margin.left = Val::Px(value.map(|v| v * REM).unwrap_or(1.0))
+                    style.margin.left = Val::Px(value.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0))
                 }
                 "mx-0" | "mx-px" | "mx-0.5" | "mx-1" | "mx-1.5" | "mx-2" | "mx-2.5" | "mx-3"
                 | "mx-3.5" | "mx-4" | "mx-5" | "mx-6" | "mx-7" | "mx-8" | "mx-9" | "mx-10"
@@ -256,7 +267,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "mx-36" | "mx-40" | "mx-44" | "mx-48" | "mx-52" | "mx-56" | "mx-60" | "mx-64"
                 | "mx-72" | "mx-80" | "mx-96" => {
                     let number = name["mx-".len()..].parse::<f32>();
-                    let value = Val::Px(number.map(|v| v * REM).unwrap_or(1.0));
+                    let value = Val::Px(number.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0));
                     style.margin.left = value;
                     style.margin.right = value;
                 }
@@ -266,7 +277,7 @@ impl Stylesheet for DefaultStyleSheet {
                 | "my-36" | "my-40" | "my-44" | "my-48" | "my-52" | "my-56" | "my-60" | "my-64"
                 | "my-72" | "my-80" | "my-96" => {
                     let number = name["my-".len()..].parse::<f32>();
-                    let value = Val::Px(number.map(|v| v * REM).unwrap_or(1.0));
+                    let value = Val::Px(number.map(|v| v * PIXEL_MULTIPLIER).unwrap_or(1.0));
                     style.margin.top = value;
                     style.margin.bottom = value;
                 }
